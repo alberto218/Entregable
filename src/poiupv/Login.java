bd1ec097777447fcb4774d514e8ff5a1eccf385c
providiego <diegomln.28@gmail.com>
2025-05-23 19:06:27 +0200

    clase e interfaz login creadas

--- src/poiupv/Login.java
+++ src/poiupv/Login.java
@@ -4,10 +4,25 @@
  */
 package poiupv;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author Diego Molina
  */
 public class Login {
+
+    @FXML
+    private Label registrarse;
+    @FXML
+    private TextField usuarioIniciar;
+    @FXML
+    private PasswordField contraseñaIniciar;
+    @FXML
+    private Button botonIniciarsesion;
     
 }
